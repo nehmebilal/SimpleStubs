e28703be3ee23739d840714e4545da8859e95507
Nehme Bilal <nbilal@microsoft.com>
2017-02-27 10:39:29 -0800

    Setting up a console app for debugging

--- test/SimpleStubs.DebugApp/Properties/AssemblyInfo.cs
+++ test/SimpleStubs.DebugApp/Properties/AssemblyInfo.cs
@@ -5,25 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-
-[assembly: AssemblyTitle("SimpleStubs")]
+[assembly: AssemblyTitle("SimpleStubs.DebugApp")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Etg.SimpleStubs.CodeGen")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("SimpleStubs.DebugApp")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
-[assembly: Guid("3e9c520a-94cf-46d0-864b-4293d439c92a")]
+[assembly: Guid("d7fa2430-f7de-4a66-9085-a6e8c00245e9")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -35,7 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: InternalsVisibleTo("TestClassLibraryTest")]
-[assembly: InternalsVisibleTo("SimpleStubs.DebugApp")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
